b605c116815efcb91bda7dfedfb8b71a83dce6a4
psk_dream <pongsakorn_s@brainergy.digital>
2023-04-09 02:08:41 +0700

    update cv

--- src/data.ts
+++ src/data.ts
@@ -1,5 +1,5 @@
 export const fullVersionLink = "https://resume.narze.live"
-export const sourceLink = "https://github.com/narze/resume"
+export const sourceLink = "https://github.com/PSKdream/resume"
 export const ogImageUrl =
   "https://og-image.vercel.app/Resumette.png?theme=light&md=1&fontSize=200px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fvercel-triangle-black.svg"
 
@@ -112,15 +112,34 @@ export const projects = [
 
 export const researchs = [
   {
-    name: "Fracture Detection and Localization for long bones by X-ray images in JPEG format",
+    name: "The Evaluation of Bone Fracture Detection of YOLO Series",
     details:"In this paper, we comprehensively study the performance of YOLO-X and YOLO-R under fracture bone X-ray images to locate the fracture locations.",
-    present: 'The 37th International Technical Conference on Circuits/System, Computers and Communications'
+    present: 'The 37th International Technical Conference on Circuits/System, Computers and Communications',
+    url: "https://ieeexplore.ieee.org/document/9895016/",
   },
   {
     name: "Investigation of Multi-agent reinforcement learning on merge ramp for avoiding car crash on highway",
     details:"In this paper, we take the as length of prohibiting area and length of entrance area into the account to evaluate the performance of MARL. As a result, the MARL can avoid the car crashing effectively while achieve high rewards and high vehicle speed.",
-    present: 'The 37th International Technical Conference on Circuits/System, Computers and Communications'
-  }
+    present: 'The 37th International Technical Conference on Circuits/System, Computers and Communications',
+    url: "https://ieeexplore.ieee.org/document/9895011/",
+  },
+  {
+    name: "Analysis of Modern Image Classification Platforms for Bone Fracture Detection",
+    details: `in this paper, we present a
+    comprehensive evaluation of the YOLO-X, YOLO-R
+    and YOLOv7 performance under bone fracture X-ray
+    images. From the simulation results, according to the
+    head decoupled process, anchor-free and Mosaic and
+    MixUp augmentation strategies, the YOLO-X presents
+    the better detection performance than the YOLO-R
+    and YOLOv7 whose detected fracture according to the
+    implicit and explicit knowledge extraction. Moreover,
+    YOLO-X presents faster convergence speed than
+    YOLO-R and YOLOv7 significantly determined by
+    the training curves.`,
+    present: '2022 6th International Conference on Information Technology (InCIT)',
+    url: "https://ieeexplore.ieee.org/document/10067836",
+  },
 ]
 
 export const references = [
